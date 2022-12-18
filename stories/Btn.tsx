b30cd5242f98b7cc7f67f597025869e67b8f5913
진현주 <jinhh501@naver.com>
2022-12-18 16:12:07 +0900

    feat: add test button

--- stories/Btn.tsx
+++ stories/Btn.tsx
@@ -1,22 +1,23 @@
 import './btn.css';
 
 interface BtnProps {
-    primary?: boolean;
+    // primary?: boolean;
     size: 'small' | 'medium' | 'large';
     label: string;
     onClick?: () => void;
 }
 
 export const Btn = ({
-    primary = false,
+    // primary = false,
     size = 'large',
     label
 }: BtnProps) => {
-    const mode = primary ? 'btn--primary' : 'btn--secondary';
+    // const mode = primary ? 'btn--primary' : 'btn--secondary';
     return (
         <button
             type="button"
-            className={['btn', `btn--${size}`, mode].join(' ')}
+            // className={['btn', `btn--${size}`, mode].join(' ')}
+            className={['btn', `btn--${size}`].join(' ')}
         >
             {label}
         </button>
