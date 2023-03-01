9547e9193dac5e7f8c10156db7b087da31ff1762
realzu <jinhh501@naver.com>
2023-03-01 21:33:19 +0900

    fix: next.js og

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,5 +2,13 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <head>
+        <meta property='og:title' content='Realzu네 깃허브' />
+        <meta property='og:description' content='여기는 realzu.github.io' />
+      </head>
+      <Component {...pageProps} />
+    </>
+  )
 }
