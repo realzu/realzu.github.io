e3e1520b9f7ce593dd77b88b77b7f48b54f84bfe
realzu <jinhh501@naver.com>
2023-03-01 22:05:43 +0900

    chore: move

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,5 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <meta property='og:title' content='Realzu네 깃허브' />
-        <meta property='og:description' content='여기는 realzu.github.io' />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
