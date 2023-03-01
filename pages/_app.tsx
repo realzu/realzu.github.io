de73a6dc6d58bf1f23a01e33e1cb79c7c2c8398e
realzu <jinhh501@naver.com>
2023-03-01 22:10:11 +0900

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,5 +3,13 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta property='og:title' content='Realzu네 깃허브' />
+        <meta property='og:description' content='여기는 realzu.github.io' />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
