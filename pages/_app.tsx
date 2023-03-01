e1de1fcf2e963b84b9d7747db728b428dfd32823
realzu <jinhh501@naver.com>
2023-03-01 21:45:53 +0900

    chore: add page, fix head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <head>
+      <Head>
         <meta property='og:title' content='Realzu네 깃허브' />
         <meta property='og:description' content='여기는 realzu.github.io' />
-      </head>
+      </Head>
       <Component {...pageProps} />
     </>
   )
---
+++ pages/sub.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const sub = () => {
+    return (
+        <div>
+            sub 페이지
+        </div>
+    );
+};
+
+export default sub;
